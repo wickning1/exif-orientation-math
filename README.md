76266896a2affc4224eef61a033f55a39c24bd26
Nick Wing <git@mythgaming.net>
2019-08-07 08:08:30 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # exif-orientation-math
-Small library to compose multiple exif orientation values and convert an orientation into flip and rotate instructions.
+Small library to compose multiple exif orientation values and convert an
+orientation into flip and rotate instructions.
+
+## Methods
+### compose (Number, Number) -> Number
+Given two EXIF orientation numbers (1-8), returns the EXIF orientation of the
+result of doing both operations in sequence. This could be useful if you are
+applying an additional EXIF orientation (say, from a database) on top of what
+is stored in a file.
+```
+const exifmath = require('exif-orientation-math')
+const neworientation = exifmath.compose(5, 7)
+// neworientation: 3
+```
+
+### operations (Number) -> { flip: Boolean, angle: Number }
+Given an EXIF orientation, returns the meaning in an object defining whether or
+not to flip first, and a rotation angle to apply after the flip.
+```
+const exifmath = require('exif-orientation-math')
+const operations = exifmath.operations(5)
+// operations: { flip: true, angle: 270 }
---
+++ index.js
@@ -0,0 +1,75 @@
+module.exports = {
+  compose: function (first, second) {
+    if (first === 1) return second
+    if (second === 1) return first
+    if (first === 2) {
+      if (second === 2) return 1
+      if (second === 3) return 4
+      if (second === 4) return 3
+      if (second === 5) return 6
+      if (second === 6) return 5
+      if (second === 7) return 8
+      if (second === 8) return 7
+    }
+    if (first === 3) {
+      if (second === 2) return 4
+      if (second === 3) return 1
+      if (second === 4) return 2
+      if (second === 5) return 7
+      if (second === 6) return 8
+      if (second === 7) return 5
+      if (second === 8) return 6
+    }
+    if (first === 4) {
+      if (second === 2) return 3
+      if (second === 3) return 2
+      if (second === 4) return 1
+      if (second === 5) return 8
+      if (second === 6) return 7
+      if (second === 7) return 6
+      if (second === 8) return 5
+    }
+    if (first === 5) {
+      if (second === 2) return 8
+      if (second === 3) return 6
+      if (second === 4) return 7
+      if (second === 5) return 1
+      if (second === 6) return 4
+      if (second === 7) return 3
+      if (second === 8) return 2
+    }
+    if (first === 6) {
+      if (second === 2) return 7
+      if (second === 3) return 8
+      if (second === 4) return 5
+      if (second === 5) return 2
+      if (second === 6) return 3
+      if (second === 7) return 4
+      if (second === 8) return 1
+    }
+    if (first === 7) {
+      if (second === 2) return 6
+      if (second === 3) return 5
+      if (second === 4) return 8
+      if (second === 5) return 3
+      if (second === 6) return 2
+      if (second === 7) return 1
+      if (second === 8) return 4
+    }
+    if (first === 8) {
+      if (second === 2) return 5
+      if (second === 3) return 6
+      if (second === 4) return 7
+      if (second === 5) return 4
+      if (second === 6) return 1
+      if (second === 7) return 2
+      if (second === 8) return 3
+    }
+  },
+  operations: function (orientation) {
+    const o = parseInt(orientation)
+    const flip = [2, 4, 5, 7].includes(o)
+    const angle = [0, 0, 180, 180, 270, 270, 90, 90][o]
+    return { flip, angle }
+  }
+}
